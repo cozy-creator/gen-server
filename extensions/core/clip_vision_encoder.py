703f34c792d7461d108e67d9b1fb72ea5959a0ee
Leommm-byte <leom20031@gmail.com>
2024-05-08 13:59:00 +0100

    Added some nodes

--- extensions/core/clip_vision_encoder.py
+++ extensions/core/clip_vision_encoder.py
@@ -1,1 +1,21 @@
 # takes an image and produces an embedding
+class CLIPVisionEncode:
+
+
+    NAMESPACE = "clip_vision"
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": { "clip_vision": ("CLIP_VISION",),
+                              "image": ("IMAGE",)
+                             }}
+    RETURN_TYPES = ("CLIP_VISION_OUTPUT",)
+    FUNCTION = "encode"
+
+    CATEGORY = "conditioning"
+
+    def encode(self, clip_vision, image):
+        output = clip_vision.encode_image(image)
+        return (output,)
+    
+
--- extensions/core/const.py
+++ extensions/core/const.py
@@ -1,1 +1,15 @@
 # Has no inputs. Outputs a constant value.
+
+
+CONSTANT_VALUE = 0  # The value to output
+class ConstantNode:
+    def __init__(self):
+        pass
+
+    RETURN_TYPES = ("*",)  
+    FUNCTION = "pass_through"
+
+    CATEGORY = "misc"
+
+    def pass_through(self):
+        return CONSTANT_VALUE  # Returns the constant value
