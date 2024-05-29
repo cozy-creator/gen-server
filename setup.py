3e59fff330afcd41af2a81a9f65862de79e0e61b
Paul Fidika <paul@fidika.com>
2024-05-29 02:58:41 -0500

    add setup.py meta-package

--- setup.py
+++ setup.py
@@ -1,23 +1,16 @@
-from setuptools import setup, find_packages
+# this is a meta package for distribution
 
-# Note that if there are any dev / test packages specified in the requirements.txt
-# file, you should exclude them from being added here.
-with open('requirements.txt') as file:
-    required = file.read().splitlines()
+from setuptools import setup
 
 setup(
     name="ComfyCreator",
     version="0.0.1",
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'comfy-creator = gen_server.main:main'
-        ],
-        'comfy_creator.extensions': [
-            'extension1 = core:SomeClass',
-            'extension2 = core2.main:main',
-            'extension3 = image_nodes.:AnotherClass',
-        ]
-    },
-    install_requires=required
+    description="Install gen_server and its extensions",
+    install_requires=[
+        'gen_server==1.0.0',
+        'ComfyCreator-extension1==0.1.0',
+        'ComfyCreator-extension2==0.1.0',
+        'ComfyCreator-extension3==0.1.0',
+        # Add other extensions as needed
+    ]
 )
