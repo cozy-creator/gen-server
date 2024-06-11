deecb44c76c29f5f4aed6c460f0cd98b20535768
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2024-06-11 15:24:40 +0100

    pass node id into node

--- packages/gen_server/src/gen_server/workflow/nodes/base.py
+++ packages/gen_server/src/gen_server/workflow/nodes/base.py
@@ -6,10 +6,14 @@ class NodeType(Enum):
 
 
 class BaseNode:
+    id: str
     inputs: dict = {}
     outputs: dict = {}
 
     type: NodeType
 
-    def run(self, state):
+    def __init__(self, node_id: str):
+        self.id = node_id
+
+    def __call__(self, state):
         raise NotImplementedError
--- packages/gen_server/src/gen_server/workflow/runner.py
+++ packages/gen_server/src/gen_server/workflow/runner.py
@@ -1,6 +1,6 @@
 from typing import TypedDict, Union
 
-from .nodes.base import NodeType
+from .nodes.base import NodeType, BaseNode
 from .nodes.registry import node_registry
 from .state import RunnerState
 
@@ -37,16 +37,16 @@ class WorkflowExecutor:
 
         return sorted_nodes
 
-    def _get_node(self, data: WorkflowData):
+    def _get_node(self, node_id: str, data: WorkflowData):
         if not data.get("type"):
             raise ValueError('node type is required')
 
-        Node = node_registry.get(data.get("type"))
-        return Node(data)
+        node_class = node_registry.get(data.get("type"))
+        return node_class(node_id)
 
-    async def execute_node(self, node):
+    async def execute_node(self, node: BaseNode):
         try:
-            await node.execute(self.state)
+            await node(self.state)
         except Exception as error:
             # raise WorkflowNodeRunFailedError(node)
             raise error
@@ -61,7 +61,7 @@ class WorkflowExecutor:
 
         for node_id in node_ids:
             try:
-                node = self._get_node(self.workflow[node_id])
+                node = self._get_node(node_id, self.workflow[node_id])
                 if not node:
                     break
                 await self.execute_node(node)
