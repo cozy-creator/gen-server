148c66362e5dff17b8ce05a0d2d84832da454a19
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2024-06-18 16:03:16 +0100

    update `load_checkpoints`

--- packages/gen_server/src/gen_server/base_types/checkpoint.py
+++ packages/gen_server/src/gen_server/base_types/checkpoint.py
@@ -1,35 +1,5 @@
-from typing import Dict, Type
-from unittest.mock import Base
+from typing import Dict
 from .architecture import Architecture
-
-
-# key in dictionary needs to be unique; we need a unique identifier
-# maybe internal identifier? Randomly generated? maybe full file path?
-# maybe also have file-sycnhronization
-# this will be different than what we send to the client
-# "sdxl_base.fp16.safetensors" ->
-# {
-#     components: {
-#         'vae': Vae_architecture,
-#         'text_encoder': CLIP,
-#         'text_decoder_2': CLIPWithPojection
-#         'unet': SDXLUnet
-#     },
-#      file_path: '~/.comfy-creator/models/sdxl_base.fp16.safetensors',
-#     dtype: torch.dtype.fp16,
-#     group: 'sdxl',
-#     display_name: 'SDXL Base',
-#     author: ,
-#     date: ,
-#     (other metadata taken from the header)
-# }
-
-# SDXL models:
-#     - SDXL Base
-#     - Dreamshaper xlogy
-# SD3 models:
-#     - Base
-#     - Fine Tuned 5
 
 
 class Checkpoint:
@@ -38,10 +8,36 @@ class Checkpoint:
     Does PyTorch already have something along these lines?
     """
 
-    def __init__(self):
-        self._components: Dict[str, Architecture] = {}
-        self._display_name: str = 'something'
+    def __init__(
+        self,
+        display_name: str,
+        components: Dict[str, Architecture] = None,
+        metadata: Dict = None,
+    ):
+        if components is None:
+            components = {}
+        self._components: Dict[str, Architecture] = components
+        self._display_name = display_name
+        """
+        The display name of the checkpoint. This is the name that will be shown in the UI.
+        """
+        self.metadata = metadata
+        """
+        Additional metadata for the checkpoint.
+        """
+
+    def add_component(self, name: str, component: Architecture):
+        """
+        Adds a component to the checkpoint.
+        """
+        if name in self._components:
+            raise ValueError(f"Component with name '{name}' already exists.")
+        self._components[name] = component
 
     @property
     def components(self) -> Dict[str, Architecture]:
         return self._components
+
+    @property
+    def display_name(self) -> str:
+        return self._display_name
