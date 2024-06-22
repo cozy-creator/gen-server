34a1c4a75b27daa8cb16c90b335ef8f220e555c7
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2024-06-22 10:36:07 +0100

    add spandrel compatibility

--- packages/gen_server/src/gen_server/base_types/architecture.py
+++ packages/gen_server/src/gen_server/base_types/architecture.py
@@ -1,14 +1,15 @@
-from typing import Any, Generic, TypeVar, Optional
-from abc import ABC, abstractmethod
+from typing import Any, Generic, TypeVar, Callable
 import torch
-from .common import TorchDevice, StateDict
+from .common import StateDict
+
+from spandrel import Architecture as SpandrelArchitecture, ArchId
 
 T = TypeVar("T", bound=torch.nn.Module, covariant=True)
 
 
 # TO DO: in the future, maybe we can compare sets of keys, rather than use
 # a detect method?
-class Architecture(ABC, Generic[T]):
+class Architecture(SpandrelArchitecture, Generic[T]):
     """
     The interface that all architecture definitions should implement.
     The construct __init__ function should accept no arguments.
@@ -16,6 +17,7 @@ class Architecture(ABC, Generic[T]):
     A wrapper class for PyTorch models that adds additional properties and methods
     for inspection and management of the model.
     """
+
     display_name: str
     input_space: str
     output_space: str
@@ -23,9 +25,20 @@ class Architecture(ABC, Generic[T]):
     def __init__(
         self,
         model: T,
+        detect: Callable[[StateDict], bool] = None,
         config: Any = None,
     ) -> None:
-        super().__init__()
+        arch_id = ArchId(
+            model.__name__ if isinstance(model, type) else type(model).__name__
+        )
+        detect = detect if detect is not None else self.detect
+        name = self.display_name if hasattr(self, "display_name") else None
+
+        super().__init__(
+            name=name,
+            id=arch_id,
+            detect=detect,
+        )
 
         self._model = model
         self._config = config
@@ -44,42 +57,43 @@ class Architecture(ABC, Generic[T]):
         """
         return self._config
 
-    @classmethod
-    @abstractmethod
-    def detect(cls, state_dict: StateDict) -> bool:
-        """
-        Detects whether the given state dictionary matches the architecture.
+    # @classmethod
+    # @abstractmethod
+    # def detect(cls, state_dict: StateDict) -> bool:
+    #     """
+    #     Detects whether the given state dictionary matches the architecture.
+    #
+    #     Args:
+    #         state_dict (StateDict): The state dictionary from a PyTorch model.
+    #
+    #     Returns:
+    #         bool: True if the state dictionary matches the architecture, False otherwise.
+    #     """
+    #     pass
 
-        Args:
-            state_dict (StateDict): The state dictionary from a PyTorch model.
+    #
+    # @abstractmethod
+    # def load(self, state_dict: StateDict, device: Optional[TorchDevice] = None) -> None:
+    #     """
+    #     Loads a model from the given state dictionary according to the architecture.
+    #
+    #     Args:
+    #         state_dict (StateDict): The state dictionary from a PyTorch model.
+    #
+    #     Returns:
+    #         torch.nn.Module: The loaded PyTorch model.
+    #     """
+    #     pass
 
-        Returns:
-            bool: True if the state dictionary matches the architecture, False otherwise.
-        """
-        pass
-
-    @abstractmethod
-    def load(self, state_dict: StateDict, device: Optional[TorchDevice] = None) -> None:
-        """
-        Loads a model from the given state dictionary according to the architecture.
-
-        Args:
-            state_dict (StateDict): The state dictionary from a PyTorch model.
-
-        Returns:
-            torch.nn.Module: The loaded PyTorch model.
-        """
-        pass
-    
     def serialize(self) -> dict[str, Any]:
         """
         Serialize the Architecture instance to a dictionary.
         """
         return {
-            'display_name': self.display_name,
+            "display_name": self.display_name,
             # 'config': self._config,
-            'input_space': self.input_space,
-            'output_space': self.output_space
+            "input_space": self.input_space,
+            "output_space": self.output_space,
         }
 
     # def __repr__(self) -> str:
