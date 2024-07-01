afc9be302736e5f5d17965ac6b53f5a19249e2a5
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2024-07-01 21:26:26 +0100

    add rmbg and image compositing

---
+++ packages/core_extension_1/src/core_extension_1/architectures/rmbg_1.4_arch/briarmbg/config.py
@@ -0,0 +1,10 @@
+from transformers import PretrainedConfig
+
+
+class RMBGConfig(PretrainedConfig):
+    model_type = "SegformerForSemanticSegmentation"
+
+    def __init__(self, in_ch=3, out_ch=1, **kwargs):
+        self.in_ch = in_ch
+        self.out_ch = out_ch
+        super().__init__(**kwargs)
