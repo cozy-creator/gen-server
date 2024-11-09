6b29f1b22a556bdc9398d8f28992d320ad54f693
Leommm-byte <leom20031@gmail.com>
2024-11-09 16:14:01 +0100

    minor update

--- python_packages/gen_server/src/gen_server/utils/model_memory_manager.py
+++ python_packages/gen_server/src/gen_server/utils/model_memory_manager.py
@@ -29,7 +29,7 @@ from ..globals import (
 from ..utils.load_models import load_state_dict_from_file
 from ..utils.utils import serialize_config
 from ..utils.quantize_models import quantize_model_fp8
-from OmniGen import OmniGenPipeline
+# from OmniGen import OmniGenPipeline
 
 # Configure logging
 logger = logging.getLogger(__name__)
